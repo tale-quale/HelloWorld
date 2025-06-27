165658f792e94304ae2071496f31202722d57e66
tale-quale <laptev.ilya@gmail.com>
2025-06-27 14:13:10 -0700

    personalisation of the greeting' message

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
-	
+	got := Hello("Ivan")
+	want := "Hello, Ivan!"
+
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
